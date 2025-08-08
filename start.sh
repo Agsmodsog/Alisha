9b4ca893563ccba00c1d18f4d581e7614618d94f
AGS MODS <agsmods5@gmail.com>
2025-08-09 01:03:16 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Agsmodsog/Alisha.git /Alisha
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Alisha
+fi
+cd /Alisha
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
+
